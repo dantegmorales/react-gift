717b6aed037b91b6328a3311b47bb855efce9be6
Dante Gaspar Morales <dantegmorales@hotmail.com>
2021-01-11 21:00:53 -0600

    Muestra una lista de categorias y se agrega la función para crear una nueva.

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,11 +1,28 @@
-
-import React from "react";
+import React, { useState } from "react";
 
 const GifExpertApp = ()=>{
+
+    //Declaramos el state que nos permite contener nuestras categorias
+    const [categories, setcategories] = useState(['Neon Genesis Evengelion','Akira','Naruto']);
+
+    //Creamos una función que nos permita agregar elementos a nuestro state
+    const handleAdd = ()=>{
+        //setcategories(['Dragon Ball',...categories]);
+        setcategories(cats => [...cats,'Dragon Ball'])
+    }
+
     return(
     <>
         <h2>GifExpertApp</h2>
         <hr></hr>
+        <button onClick={handleAdd}>Agregar</button>
+        <ol>
+            {
+                categories.map(category=>{
+                    return <li key={category}>{category}</li>
+                })
+            }
+        </ol>
     </>
     );
 }
