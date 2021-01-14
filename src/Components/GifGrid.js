64512adecb373362ae7911a8b721b51659f0eb22
Dante Gaspar Morales <dantegmorales@hotmail.com>
2021-01-13 20:32:35 -0600

    Se agrega el hook useEffect para realizar las peticiones

--- src/Components/GifGrid.js
+++ src/Components/GifGrid.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 
 export const GifGrid = ({category})=>{
+    
+    const [count,setCount]= useState(0);
+
+    //Ejecuta el hook useEffect que permite ejecutar la función cuando se carga el componente
+    useEffect( ()=>{
+        getGifs();
+    },[])
+
+    //Función que nos permite hacer la petición al API de Giphy
     const getGifs = async ()=>{
         const url = 'https://api.giphy.com/v1/gifs/trending?api_key=Ji3hUw0UWRgKS9QkgQXwnCXGgzzQP7xA&limit=10&q=naruto';
         const resp = await fetch(url);
@@ -14,10 +23,12 @@ export const GifGrid = ({category})=>{
         });
     console.log(gifs);
     }
-    getGifs();
+
     return (
     <>
+        <h3>{count}</h3>
         <h3>{category}</h3>
+        <button onClick={()=>setCount(count+1)}>Count</button>
     </>
     );
 }
