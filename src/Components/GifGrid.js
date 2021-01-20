4c74a3ccfb897907bf1cf1c234a13979ea56def3
Dante Gaspar Morales <dantegmorales@hotmail.com>
2021-01-19 23:10:36 -0600

    se quitan los states y el useEffect para agregar un custom hook

--- src/Components/GifGrid.js
+++ src/Components/GifGrid.js
@@ -1,34 +1,23 @@
-import React, { useState,useEffect } from "react";
+import React from "react";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GridItem } from "./GifGridItem";
 
 export const GifGrid = ({category})=>{
-    
-    const [count,setCount]= useState(0);
 
-    //Ejecuta el hook useEffect que permite ejecutar la función cuando se carga el componente
-    useEffect( ()=>{
-        getGifs();
-    },[])
 
-    //Función que nos permite hacer la petición al API de Giphy
-    const getGifs = async ()=>{
-        const url = 'https://api.giphy.com/v1/gifs/trending?api_key=Ji3hUw0UWRgKS9QkgQXwnCXGgzzQP7xA&limit=10&q=naruto';
-        const resp = await fetch(url);
-        const {data} = await resp.json();
-        const gifs =  data.map(img=>{
-            return{
-                id:img.id,
-                title:img.title,
-                url:img.images.downsized_medium.url
-            }
-        });
-    console.log(gifs);
-    }
+    const {data:images,loading} = useFetchGifs(category);
 
     return (
     <>
-        <h3>{count}</h3>
-        <h3>{category}</h3>
-        <button onClick={()=>setCount(count+1)}>Count</button>
+        <h3 className="animate__animated animate__fadeIn animate__slower">{category}</h3>
+        {loading && <h3 className="animate__animated animate__flash animate__slower">Loading...</h3>}
+        <div className="card-grid">
+                {images.map((img)=>
+                (<GridItem 
+                    key={img.id} 
+                    {...img} 
+                />))}
+        </div>
     </>
     );
 }
